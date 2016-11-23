0b652c9da87e722dfb5f4679a1f06ae4103ddee3
Alex <amaxwell@Alexanders-MacBook-Pro.local>
2016-11-22 19:56:09 -0500

    Form and value intialization

--- js/sensors.js
+++ js/sensors.js
@@ -1,20 +1,36 @@
 /*
 Sensors.
 */
-var startSimulation = function() {
-  var form = document.getElementById("num_of_sensor_form");
-  var num_of_sensors = form.elements[0].value;
-  console.log("number of sensors: " + num_of_sensors);
-  runSimulation(num_of_sensors);
+
+function node(radius, xStartPos) {
+    this.radius = radius;
+    this.x = xStartPos;
 }
 
 var resetSimulation = function() {
   console.log("reset");
-  // clear the graphic
+  document.getElementById("unit_interval").value = 0;
+  document.getElementById("radius").value = 0;
+  document.getElementById("num_of_sensors").value = 0;
+    // clear the graphic
+}
+
+var runSimulation = function() {
+  console.log("run simulation");
+  var unitInterval = document.getElementById("unit_interval").value;
+  var radius = document.getElementById("radius").value;
+  var numberOfSensors = document.getElementById("num_of_sensors").value;
+  console.log(unitInterval +" " + radius + " " + numberOfSensors);
+  var nodes = [];
+  for (i = 0; i < numberOfSensors; i++) {
+    var randomStartPosition = Math.random() * unitInterval;
+    nodes[i] = new node(radius, randomStartPosition);
+  }
+  runAlgorithm(nodes, unitInterval);
 }
 
 
-var runSimulation = function() {
-// Render the D3 nodes, and run the algo.
+var runAlgorithm = function(nodes, interval) {
+  //start the algo
 
 }
